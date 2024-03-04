66cd9513be3b62b1a876f6182a5a570e8d68798f
John Dutchover <johndutchover@users.noreply.github.com>
2024-03-03 22:22:00 -0500

    Stack trace from the terraform-provider-snowflake_v0.87.0 plugin

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,13 +1,32 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
+provider "registry.terraform.io/hashicorp/tls" {
+  version = "4.0.5"
+  hashes = [
+    "h1:e4LBdJoZJNOQXPWgOAG0UuPBVhCStu98PieNlqJTmeU=",
+    "h1:jb/Rg9inGYp4t8HtBoETESsQJgdmOHoe1bzzg2uNB3w=",
+    "h1:yLqz+skP3+EbU3yyvw8JqzflQTKDQGsC9QyZAg+S4dg=",
+    "h1:zeG5RmggBZW/8JWIVrdaeSJa0OG62uFX5HY1eE8SjzY=",
+    "zh:01cfb11cb74654c003f6d4e32bbef8f5969ee2856394a96d127da4949c65153e",
+    "zh:0472ea1574026aa1e8ca82bb6df2c40cd0478e9336b7a8a64e652119a2fa4f32",
+    "zh:1a8ddba2b1550c5d02003ea5d6cdda2eef6870ece86c5619f33edd699c9dc14b",
+    "zh:1e3bb505c000adb12cdf60af5b08f0ed68bc3955b0d4d4a126db5ca4d429eb4a",
+    "zh:6636401b2463c25e03e68a6b786acf91a311c78444b1dc4f97c539f9f78de22a",
+    "zh:76858f9d8b460e7b2a338c477671d07286b0d287fd2d2e3214030ae8f61dd56e",
+    "zh:a13b69fb43cb8746793b3069c4d897bb18f454290b496f19d03c3387d1c9a2dc",
+    "zh:a90ca81bb9bb509063b736842250ecff0f886a91baae8de65c8430168001dad9",
+    "zh:c4de401395936e41234f1956ebadbd2ed9f414e6908f27d578614aaa529870d4",
+    "zh:c657e121af8fde19964482997f0de2d5173217274f6997e16389e7707ed8ece8",
+    "zh:d68b07a67fbd604c38ec9733069fbf23441436fecf554de6c75c032f82e1ef19",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
 provider "registry.terraform.io/snowflake-labs/snowflake" {
   version     = "0.87.0"
   constraints = "0.87.0"
   hashes = [
-    "h1:/suDYlGG/R8H/wh1UO9az4jCKKyeHtyYyrpd8ywm3lE=",
-    "h1:ECDGqw3EFxPiQmYa2OMKwVMWUteNvkjyg5osPJD8jyw=",
-    "h1:QcgUD9aKNls6TtaazAILZI/2FG7LM/fhXV5biTBqxbk=",
     "h1:ZLNMhndrkHtCC1C5s02oxL7mXW7x1oRBtaDyiqTuvNk=",
     "zh:0fff8302d942d59bb8edcfff6ed56b21ffa104d95ed7b4361efd7a1f62c78dbb",
     "zh:11c17c4f656b201fca397fe36f0daeb4436462998dde2a23eedc060651faec48",
--- main.tf
+++ main.tf
@@ -1,7 +1,20 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.87.0"
+    }
+  }
+}
+
+provider "snowflake" {
+  role        = "SYSADMIN"
+  private_key = file("~/.ssh/snowflake_key")
+}
+
 provider "snowflake" {
   alias = "security_admin"
-  role = "SYSADMIN"
-  private_key = file("~/.ssh/snowflake_key")
+  role  = "SECURITYADMIN"
 }
 
 resource "snowflake_role" "role" {
@@ -9,42 +22,32 @@ resource "snowflake_role" "role" {
   name     = "TF_DEMO_SVC_ROLE"
 }
 
-resource "snowflake_grant_privileges_to_role" "database_grant" {
-  provider   = snowflake.security_admin
-  privileges = ["USAGE"]
-  role_name  = snowflake_role.role.name
-  on_account_object {
-    object_type = "DATABASE"
-    object_name = snowflake_database.db.name
-  }
+resource "snowflake_grant_privileges_to_account_role" "database_grant" {
+  account_role_name = snowflake_role.role.name
+  on_account        = true
+  privileges        = ["USAGE"]
 }
 
 resource "snowflake_schema" "schema" {
-  name      = "TF_DEMO"
-  database  = "TF_DEMO"
+  name                = "TF_DEMO"
+  database            = "TF_DEMO"
   is_transient        = false
   is_managed          = false
   data_retention_days = 1
 }
 
-resource "snowflake_grant_privileges_to_role" "schema_grant" {
-  provider   = snowflake.security_admin
-  privileges = ["USAGE"]
-  role_name  = snowflake_role.role.name
-  on_schema {
-    schema_name = "\"${snowflake_database.db.name}\".\"${snowflake_schema.schema.name}\""
-  }
+resource "snowflake_grant_privileges_to_account_role" "schema_grant" {
+  account_role_name = snowflake_role.role.name
+  on_account        = true
+  privileges        = ["USAGE"]
 }
 
-resource "snowflake_grant_privileges_to_role" "warehouse_grant" {
-  provider   = snowflake.security_admin
-  privileges = ["USAGE"]
-  role_name  = snowflake_role.role.name
-  on_account_object {
-    object_type = "WAREHOUSE"
-    object_name = snowflake_warehouse.warehouse.name
-  }
+resource "snowflake_grant_privileges_to_account_role" "warehouse_grant" {
+  account_role_name = snowflake_role.role.name
+  on_account        = true
+  privileges        = ["USAGE"]
 }
+
 resource "tls_private_key" "svc_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
@@ -57,14 +60,10 @@ resource "snowflake_user" "user" {
   default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
   rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 }
-resource "snowflake_grant_privileges_to_role" "user_grant" {
-  provider   = snowflake.security_admin
-  privileges = ["MONITOR"]
-  role_name  = snowflake_role.role.name
-  on_account_object {
-    object_type = "USER"
-    object_name = snowflake_user.user.name
-  }
+resource "snowflake_grant_privileges_to_account_role" "user_grant" {
+  account_role_name = snowflake_role.role.name
+  on_account        = true
+  privileges        = ["ACCOUNTADMIN"]
 }
 
 resource "snowflake_role_grants" "grants" {
@@ -78,5 +77,5 @@ resource "snowflake_database" "db" {
 }
 
 resource "snowflake_warehouse" "warehouse" {
-  name           = "TF_DEMO"
+  name = "TF_DEMO"
 }
