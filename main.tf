6b97dfaeb21d8b4c04512736bc3f6dad27772f44
John Dutchover <johndutchover@users.noreply.github.com>
2024-03-05 11:50:00 -0500

    cleanup auth

--- main.tf
+++ main.tf
@@ -1,23 +1,30 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
+      source  = "snowflake-Labs/snowflake"
       version = "0.87.0"
     }
   }
 }
 
 provider "snowflake" {
-  role = "SYSADMIN"
-  private_key = file("~/.ssh/snowflake_key")
+  user                   = var.snowflake_tf_user
+  account                = var.snowflake_account
+  private_key            = file(var.snowflake_tf_user_pke_path)
+  private_key_passphrase = var.snowflake_tf_user_pke_pass
+  role                   = var.snowflake_tf_user_role
+  authenticator          = var.snowflake_tf_user_authenticator
 }
 
 resource "snowflake_schema" "schema" {
-  name      = "TF_DEMO"
-  database  = "TF_DEMO"
+  name                = "TF_DEMO"
+  database            = "TF_DEMO"
   is_transient        = false
   is_managed          = false
   data_retention_days = 1
+  depends_on          = [snowflake_warehouse.warehouse, snowflake_database.db]
 }
 
 resource "snowflake_database" "db" {
@@ -26,4 +33,5 @@ resource "snowflake_database" "db" {
 
 resource "snowflake_warehouse" "warehouse" {
   name           = "TF_DEMO"
+  warehouse_size = "xsmall"
 }
